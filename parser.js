f2219f2bdde6f62d628197de151e1d285cc4b3a2
Philip Mey <philipmey@gmail.com>
2016-08-18 14:02:05 +0000

    Quick and dirty method used in splitting header info.

--- parser.js
+++ parser.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/api/whoami', function (req, res) {
-    console.log(req.headers['accept-language'][0]);
-    console.log(req.trailers);
+    var softwareString = req.headers['user-agent'].split('(')[1].split(')')[0];
+    var languageString = req.headers['accept-language'].split(',')[0];
     
-    res.send({"ipaddress": req.headers['x-forwarded-for'], "language": req.headers['accept-language'], "software": req.headers['user-agent']});
+    
+    res.send({"ipaddress": req.headers['x-forwarded-for'], "language": languageString, "software": softwareString});
 });
 
 app.listen(8080, function () {
